be29e32bb0311a7965480b9e80d8501312a2ad5a
1579404542@qq.com <1579404542@qq.com>
2019-08-16 08:22:31 +0800

    完善demo190814

--- shiroCas/shiro/demo190814/src/main/java/com/lhb/controller/IndexController.java
+++ shiroCas/shiro/demo190814/src/main/java/com/lhb/controller/IndexController.java
@@ -2,8 +2,11 @@ package com.lhb.controller;
 
 import java.io.PrintWriter;
 
+
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("")
 public class IndexController {
+	private final Logger log = LogManager.getLogger();
+	
 	/**
 	 * 登录
 	 * @return  返回结果值
@@ -19,6 +24,8 @@ public class IndexController {
 	@ResponseBody
 	@GetMapping(value="/login")
 	public String login(HttpServletResponse rep){
+		log.error("==controller层的日志信息==");
+		log.info("==controller层的日志信息==");
 		String result = "使用springmvc中的类设置编码方式";
 		try {
 			/*rep.setCharacterEncoding("UTF-8");*/
@@ -27,6 +34,10 @@ public class IndexController {
 			w.write("使用流的方式进行返回");
 			w.flush();
 			w.close();*/
+			
+			String path=System.getProperty("catalina.home");
+			log.error("tomcat的访问路径==controller层的日志信息=="+path);
+			log.info("tomcat的访问路径==controller层的日志信息=="+path);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- shiroCas/shiro/demo190814/src/test/java/demo190814/Demo.java
+++ shiroCas/shiro/demo190814/src/test/java/demo190814/Demo.java
@@ -1,17 +1,31 @@
 package demo190814;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="classpath:spring/application.xml")
 public class Demo {
-	private final Logger log = LoggerFactory.getLogger(Demo.class);
+	// private final Logger log = LoggerFactory.getLogger(Demo.class);
 	
 	@Test
 	public void test(){
+		Logger log = LogManager.getLogger();
 		log.error("============test========LHB");
+		log.info("============test========LHB");
+		
+		org.slf4j.Logger log2 = LoggerFactory.getLogger(Demo.class);
+		log2.error("============test2========LHB");
+		log2.info("============test2========LHB");
+		
+		String path=System.getProperty("catalina.home");
+		log.error("tomcat的访问路径==controller层的日志信息=="+path);
+		log.info("tomcat的访问路径==controller层的日志信息=="+path);
 	}
 }
